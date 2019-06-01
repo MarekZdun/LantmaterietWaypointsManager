505751e02c4c258dc83aba411802565d52f8ae0c
buryrydz <buryrydz@gmail.com>
2019-06-01 09:46:41 +0200

    minor fix

--- src/app/containers/map.js
+++ src/app/containers/map.js
@@ -262,7 +262,6 @@ class Map extends Component {
                 //     }       
                 // }.bind(this));
                 
-                delete parentComponent.createMapManager;
                 return this;
             },
     
